ef179e670f019057d3ea2efe211d3db44ec1eab5
Stefan Landvogt <stefan.landvogt@pentlandfith.com>
2017-05-25 12:08:29 +0200

    PHP

---
+++ process.php
@@ -0,0 +1,19 @@
+<?php
+	require_once "recaptchalib.php";
+    $to = "stefan.landvogt@pentlandfirth.com"; 
+    $from = $_REQUEST['email']; 
+    $name = $_REQUEST['name']; 
+    $headers = "From: $from"; 
+    $subject = "EASY Shopper Website Form Kontakt"; 
+ 
+    $fields = array(); 
+    $fields{"name"} = "name"; 
+    $fields{"email"} = "email"; 
+    $fields{"phone"} = "phone"; 
+    $fields{"message"} = "message";
+ 
+    $body = "Here is what was sent:\n\n"; foreach($fields as $a => $b){   $body .= sprintf("%20s: %s\n",$b,$_REQUEST[$a]); }
+ 
+    $send = mail($to, $subject, $body, $headers);
+ 
+?>
